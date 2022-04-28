57e8f22072352b024c102f7d939479e385c82511
DESKTOP-ASUS\boyo_ <georgiev.boyko@gmail.com>
2022-04-28 09:55:59 +0300

    Updated urls file

--- NutritionPlatform/urls.py
+++ NutritionPlatform/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+import NutritionPlatform
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('NutritionPlatform.main.urls')),
+    path('', include('NutritionPlatform.accounts.urls')),
+    path('', include('NutritionPlatform.nutrients.urls')),
+    path('', include('NutritionPlatform.user_data.urls')),
 ]
